47a94f70d06186dc760ca45e90e73a67b28dbacb
cmartema <cjm2301@columbia.edu>
2023-09-28 01:29:21 -0400

    bfs-implementation

--- puzzle.py
+++ puzzle.py
@@ -1,6 +1,7 @@
 from __future__ import division
 from __future__ import print_function
 
+
 import sys
 import math
 import time
@@ -50,13 +51,11 @@ class PuzzleState(object):
         if self.blank_index >= self.n:  # Check if blank tile can move up by verifying its not on the top row of the board (n needs to be )
             config_copy = self.config[:]
             new_blank_index = self.blank_index - self.n
-            config_copy[self.blank_index]
-            config_copy[new_blank_index] = config_copy[new_blank_index]
-            config_copy[self.blank_index]
+            config_copy[self.blank_index], config_copy[new_blank_index] = config_copy[new_blank_index],config_copy[self.blank_index]
 
-            return PuzzleState(config_copy, self.n, parent=self, action="Up", cost=self.cost + 1)
+            return PuzzleState(config_copy, self.n, self.config,"Up", self.cost + 1)
         
-        print("Invalid Move")
+        #print("Invalid Move on up")
         return None
 
     def move_down(self):
@@ -68,10 +67,10 @@ class PuzzleState(object):
             config_copy = self.config[:]
             new_blank_index = self.blank_index + self.n
             config_copy[self.blank_index], config_copy[new_blank_index] = config_copy[new_blank_index], config_copy[self.blank_index]
-
-            return PuzzleState(config_copy, self.n, parent=self, action="Down", cost=self.cost + 1)
+            
+            return PuzzleState(config_copy, self.n, self.config, "Down", self.cost + 1)
         
-        print("Invalid Move")
+        #print("Invalid Move on down")
         return None
         
     def move_left(self):
@@ -79,14 +78,14 @@ class PuzzleState(object):
         Moves the blank tile one column to the left.
         :return a PuzzleState with the new configuration
         """
-        if self.blank_index % self.n != 0:  # Check if blank tile can move left
+        if self.blank_index % self.n != 0: # Check if blank tile can move left
             config_copy = self.config[:]
             new_blank_index = self.blank_index - 1
             config_copy[self.blank_index], config_copy[new_blank_index] = config_copy[new_blank_index], config_copy[self.blank_index]
 
-            return PuzzleState(config_copy, self.n, parent=self, action="Left", cost=self.cost + 1)
+            return PuzzleState(config_copy, self.n, self.config, "Left", self.cost + 1)
 
-        print("Invalid Move")
+        #print("Invalid Move on left")
         return None
     
     def move_right(self):
@@ -95,13 +94,13 @@ class PuzzleState(object):
         :return a PuzzleState with the new configuration
         """
         if (self.blank_index + 1) % self.n != 0:  # Check if blank tile can move right
-            new_config = self.config[:]
+            config_copy = self.config[:]
             new_blank_index = self.blank_index + 1
-            new_config[self.blank_index], new_config[new_blank_index] = new_config[new_blank_index], new_config[self.blank_index]
+            config_copy[self.blank_index], config_copy[new_blank_index] = config_copy[new_blank_index], config_copy[self.blank_index]
 
-            return PuzzleState(new_config, self.n, parent=self, action="Right", cost=self.cost + 1)
+            return PuzzleState(config_copy, self.n, self.config, "Right", self.cost + 1)
 
-        print("Invalid Move")
+        #print("Invalid Move on right")
         return None
       
     def expand(self):
@@ -125,26 +124,48 @@ class PuzzleState(object):
 # Function that Writes to output.txt
 
 ### Students need to change the method to have the corresponding parameters
-def writeOutput():
+def writeOutput(path_to_goal, cost_of_path, nodes_expanded, search_depth, max_search_depth, running_time, max_ram_usage):
     file_name = "output.txt"
     with open(file_name, "w") as file:
-       
-        '''
-        file.write("Hello, World!\n")
-        file.write("This is a sample file.\n")
-        file.write("You can write more lines here.")
-        '''
+        file.write(f"path to goal: {path_to_goal}\n")
+        file.write(f"cost of path: {cost_of_path}\n")
+        file.write(f"nodes expanded: {nodes_expanded}\n")
+        file.write(f"search depth: {search_depth}\n")
+        file.write(f"max search depth: {max_search_depth}\n")
+        file.write(f"running time: {running_time:.8f}\n")
+        file.write(f"max ram usage: {max_ram_usage:.8f}\n")
     
 
 def bfs_search(initial_state):
     """BFS search"""
     ### STUDENT CODE GOES HERE ###
-    pass
+    frontier = Q.Queue()
+    frontier.put(initial_state)
+    explored = set()
+
+    while not frontier.empty():
+        state = frontier.get()
+        explored.add(state)
+
+        if test_goal(state) == True:
+            return state
+        
+        child  = state.expand()
+        for neighbor in child:
+            if neighbor not in frontier.queue and neighbor not in explored:
+                frontier.put(neighbor)
+
+    return None
+        
+    
+    
+
+
 
 def dfs_search(initial_state):
     """DFS search"""
     ### STUDENT CODE GOES HERE ###
-    pass
+    bfs_search(initial_state)
 
 def A_star_search(initial_state):
     """A * search"""
@@ -164,7 +185,12 @@ def calculate_manhattan_dist(idx, value, n):
 def test_goal(puzzle_state):
     """test the state is the goal state or not"""
     ### STUDENT CODE GOES HERE ###
-    pass
+    goal_list = sorted(puzzle_state.config[:])
+    config_copy = puzzle_state.config[:]
+    if goal_list == config_copy:
+        return True
+    
+    return
 
 # Main Function that reads in Input and Runs corresponding Algorithm
 def main():
